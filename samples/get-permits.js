bbe66f90814ec1772f242812ecc37584c75959b1
Mark Headd <mheadd@gmail.com>
2016-02-03 12:53:51 -0500

    Fixed tests

--- samples/get-permits.js
+++ samples/get-permits.js
@@ -1,5 +1,5 @@
 var phli = require('../lib/phli')();
 
-phli.getPermitInfo(659695, { expand: 'locations' }, function (err, data) {
+phli.getPermitInfo(659695, { expand: 'locations' }, function getData(err, data) {
   console.log(JSON.stringify(data));
 });
